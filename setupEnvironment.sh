4d098f7cc499c30dc360eef3d6f42aacc4dd4fc3
Yngve Levinsen <yngve.levinsen@ess.eu>
2023-05-03 08:36:42 +0200

    fix setupEnvironment script for python3 static linking not working so commented out for now

--- setupEnvironment.sh
+++ setupEnvironment.sh
@@ -3,36 +3,36 @@ command_exists () {
 }
 
 
-export ORBIT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo "ORBIT installed in $ORBIT_ROOT"
+export ORBIT3_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "ORBIT installed in $ORBIT3_ROOT"
 
-export ORBIT_ARCH=`uname -s`
+export ORBIT3_ARCH=`uname -s`
 
 # alias python='python2'
 
-if command_exists python2; then
-   PYEX=python2
+if command_exists python3; then
+   PYEX=python3
 else
    PYEX=python
 fi
 
-export PYTHON_VERSION=`$PYEX -c "from distutils import sysconfig; print sysconfig.get_config_var('VERSION');"`
-echo "Python version is $PYTHON_VERSION"
+export PYTHON3_VERSION=`$PYEX -c "from distutils import sysconfig; print(sysconfig.get_config_var('VERSION'));"`
+echo "Python version is $PYTHON3_VERSION"
 
-PYTHON_LIB_DIR=`$PYEX -c "from distutils import sysconfig; print sysconfig.get_config_var('LIBPL');"`
-if [ -f $PYTHON_LIB_DIR/libpython${PYTHON_VERSION}.a ]
-   then
-	export PYTHON_ROOT_LIB=$PYTHON_LIB_DIR/libpython${PYTHON_VERSION}.a
-	LIB_TYPE=static
-   else
-	export PYTHON_ROOT_LIB="-L $PYTHON_LIB_DIR -lpython${PYTHON_VERSION}"
-	LIB_TYPE=dynamic
-fi
+PYTHON3_LIB_DIR=`$PYEX -c "from distutils import sysconfig; print(sysconfig.get_config_var('LIBPL'));"`
+#if [ -f $PYTHON3_LIB_DIR/libpython${PYTHON3_VERSION}.a ]
+#then
+#  export PYTHON3_ROOT_LIB=$PYTHONi3_LIB_DIR/libpython${PYTHON3_VERSION}.a
+#  LIB_TYPE=static
+#else
+  export PYTHON3_ROOT_LIB="-L $PYTHON3_LIB_DIR -lpython${PYTHON3_VERSION}"
+  LIB_TYPE=dynamic
+#fi
 
-echo "Found python library: ${PYTHON_LIB_DIR} will use $LIB_TYPE library"
+echo "Found python library: ${PYTHON3_LIB_DIR} will use $LIB_TYPE library"
 
-export PYTHON_ROOT_INC=`$PYEX -c "from distutils import sysconfig; print sysconfig.get_config_var('INCLUDEPY');"`
-echo "Found Python include directory: $PYTHON_ROOT_INC"
+export PYTHON3_ROOT_INC=`$PYEX -c "from distutils import sysconfig; print(sysconfig.get_config_var('INCLUDEPY'));"`
+echo "Found Python include directory: $PYTHON3_ROOT_INC"
 
 export PYTHONPATH=${PYTHONPATH}:${ORBIT_ROOT}/py:${ORBIT_ROOT}/lib
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${ORBIT_ROOT}/lib
