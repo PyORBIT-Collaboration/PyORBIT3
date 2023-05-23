4835f0da52d6a44d0980e491ad6b94ac27fc9b4b
Yngve Levinsen <yngve.levinsen@ess.eu>
2023-05-23 22:07:40 +0200

    added MaterialInteractions from py2 version and updated it for python3

---
+++ src/orbit/MaterialInteractions/wrap_foil.hh
@@ -0,0 +1,18 @@
+#ifndef WRAP_ORBIT_FOIL_HH_
+#define WRAP_ORBIT_FOIL_HH_
+
+#include "Python.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  namespace wrap_foil{
+    PyMODINIT_FUNC initfoil();
+  }
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*WRAP_ORBIT_FOIL_HH_*/
