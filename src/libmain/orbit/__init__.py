89149d8f88e6e3c5865dab4d94a33633036cabc6
Yngve Levinsen <yngve.levinsen@ess.eu>
2023-05-24 09:20:46 +0200

    modify orbit.__init__

--- src/libmain/orbit/__init__.py
+++ src/libmain/orbit/__init__.py
@@ -15,7 +15,10 @@ def _load_module(mod_name, path):
     return module
 
 
-_top_level_modules = ["orbit_mpi", "bunch", "teapot_base", "linac", "spacecharge", "orbit_utils", "aperture"]
+__all__ = ["orbit_mpi", "bunch", "teapot_base", 
+           "linac", "spacecharge", "orbit_utils",
+           "aperture", "foil", "collimator"]
 
-for mod_name in _top_level_modules:
+for mod_name in __all__:
     locals()[mod_name] = _load_module(mod_name, pkg_path)
+del mod_name
