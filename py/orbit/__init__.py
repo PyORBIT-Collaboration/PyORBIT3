289878c6ed01d206f5c9434c132d8f4504e82ab8
Yngve Levinsen <yngve.levinsen@ess.eu>
2023-05-24 16:02:53 +0200

    edits in how version is pulled from scm better pypi building

--- py/orbit/__init__.py
+++ py/orbit/__init__.py
@@ -8,3 +8,15 @@
 ## - teapot      - the TEAPOT-like ORBIT lattice elements.
 ## - teapot_base - fundamental bricks of teapot elements.
 ## - utils       - the collection of useful Python utilites.
+
+try:
+    from importlib import metadata
+except ImportError:
+    # Running on pre-3.8 Python; use importlib-metadata package
+    import importlib_metadata as metadata  # type: ignore
+
+try:
+    __version__ = metadata.version("ess-python-tools")  # type: ignore
+except ModuleNotFoundError:
+    # package is not installed
+    pass
