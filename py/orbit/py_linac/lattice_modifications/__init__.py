8877d2c58cf4a0b6bbec109946a8ebf93ca60316
Christian Zlatanov <christian.zlatanov@ess.eu>
2023-06-22 15:09:24 +0200

    fixed some imports

--- py/orbit/py_linac/lattice_modifications/__init__.py
+++ py/orbit/py_linac/lattice_modifications/__init__.py
@@ -2,10 +2,10 @@
 ## \Classes and packages of ORBIT Linac.
 ##
 
-# from apertures_additions_lib import GetLostDistributionArr
-# from apertures_additions_lib import AddScrapersAperturesToLattice
-# from apertures_additions_lib import Add_drift_apertures_to_lattice
-# from sns_aperture_additions import AddMEBTChopperPlatesAperturesToSNS_Lattice
+from orbit.py_linac.lattice_modifications.apertures_additions_lib import GetLostDistributionArr
+from orbit.py_linac.lattice_modifications.apertures_additions_lib import AddScrapersAperturesToLattice
+from orbit.py_linac.lattice_modifications.apertures_additions_lib import Add_drift_apertures_to_lattice
+from orbit.py_linac.lattice_modifications.sns_aperture_additions import AddMEBTChopperPlatesAperturesToSNS_Lattice
 
 from orbit.py_linac.lattice_modifications.apertures_additions_lib import Add_quad_apertures_to_lattice
 from orbit.py_linac.lattice_modifications.apertures_additions_lib import Add_rfgap_apertures_to_lattice
@@ -34,7 +34,7 @@ __all__.append("Add_rfgap_apertures_to_lattice")
 # __all__.append("GetLostDistributionArr")
 # __all__.append("AddScrapersAperturesToLattice")
 # __all__.append("Add_drift_apertures_to_lattice")
-# __all__.append("AddMEBTChopperPlatesAperturesToSNS_Lattice")
+__all__.append("AddMEBTChopperPlatesAperturesToSNS_Lattice")
 __all__.append("Replace_BaseRF_Gap_to_AxisField_Nodes")
 __all__.append("Replace_BaseRF_Gap_and_Quads_to_Overlapping_Nodes")
 __all__.append("Replace_Quads_to_OverlappingQuads_Nodes")
