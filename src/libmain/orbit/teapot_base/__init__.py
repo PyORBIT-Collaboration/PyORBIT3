193bd5ed4d4aeec5ac432ddf3e349d5ca4c8583e
Juan F. Esteban Müller <JuanF.EstebanMuller@ess.eu>
2023-07-06 12:50:44 +0200

    fixing case when one wants to load a class/method directly from the lowest level module

--- src/libmain/orbit/teapot_base/__init__.py
+++ src/libmain/orbit/teapot_base/__init__.py
@@ -1,8 +1,7 @@
 from orbit.core._module_loader import load_module
 
 mod_name = __name__.split(".")[-1]
-# mod_name = "teapot_base"
 
-locals()[mod_name] = load_module(mod_name)
+locals().update(load_module(mod_name).__dict__)
 
 del mod_name
--- src/libmain/orbit/trackerrk4/__init__.py
+++ src/libmain/orbit/trackerrk4/__init__.py
@@ -2,6 +2,6 @@ from orbit.core._module_loader import load_module
 
 mod_name = __name__.split(".")[-1]
 
-locals()[mod_name] = load_module(mod_name)
+locals().update(load_module(mod_name).__dict__)
 
 del mod_name
