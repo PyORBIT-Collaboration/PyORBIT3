86e6124c5528409cd06ea4cedbec718886b7958d
austin-hoover <austin.michael.hoover@gmail.com>
2025-04-10 17:06:59 -0400

    Add plotting scripts for DanilovEnvelope20 and DanilovEnvelope22

---
+++ examples/ext/danilov_envelope/plot_danilov_envelope_20.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from orbit.danilov_envelope import DanilovEnvelope20
+
+
+envelope = DanilovEnvelope20(
+    eps_x=10.00e-06,
+    eps_y=10.00e-06,
+    mass=0.938,
+    kin_energy=1.0,
+    length=100.0,
+    intensity=0.0,
+)
+
+x = envelope.sample(100_000)
+xmax = np.std(x, axis=0) * 3.0
+limits = list(zip(-xmax, xmax))
+
+fig, axs = plt.subplots(ncols=4, nrows=4, figsize=(4, 4), constrained_layout=True)
+for i in range(4):
+    for j in range(4):
+        ax = axs[i, j]
+        ax.hist2d(x[:, j], x[:, i], bins=50, range=[limits[j], limits[i]])
+for ax in axs.flat:
+    ax.set_xticks([])
+    ax.set_yticks([])
+plt.show()
---
+++ examples/ext/danilov_envelope/plot_danilov_envelope_22.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from orbit.danilov_envelope import DanilovEnvelope22
+
+
+envelope = DanilovEnvelope22(
+    intrinsic_emittance=20.00e-06,
+    mass=0.938,
+    kin_energy=1.0,
+    length=100.0,
+    intensity=0.0,
+    mode=1,
+    params=None
+)
+
+x = envelope.sample(100_000)
+xmax = np.std(x, axis=0) * 3.0
+limits = list(zip(-xmax, xmax))
+
+fig, axs = plt.subplots(ncols=4, nrows=4, figsize=(4, 4), constrained_layout=True)
+for i in range(4):
+    for j in range(4):
+        ax = axs[i, j]
+        ax.hist2d(x[:, j], x[:, i], bins=50, range=[limits[j], limits[i]])
+for ax in axs.flat:
+    ax.set_xticks([])
+    ax.set_yticks([])
+plt.show()
