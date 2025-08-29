44411b28915be23f42941f8145870eb5ca802224
austin-hoover <austin.michael.hoover@gmail.com>
2025-08-29 17:31:18 -0400

    Initial version of BunchTuneAnalysis4D

---
+++ src/orbit/BunchDiagnostics/wrap_bunch_tune_analysis_4d.cc
@@ -0,0 +1,145 @@
+#include "orbit_mpi.hh"
+#include "pyORBIT_Object.hh"
+
+#include "wrap_bunch_tune_analysis_4d.hh"
+#include "wrap_bunch.hh"
+
+#include <iostream>
+
+#include "BunchTuneAnalysis4D.hh"
+
+
+namespace wrap_bunch_tune_analysis_4d {
+
+	void error(const char* msg){ ORBIT_MPI_Finalize(msg); }
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+Constructor for python class wrapping C++ BunchTuneAnalysis4D instance.
+It never will be called directly.
+*/
+static PyObject* BunchTuneAnalysis4D_new(PyTypeObject *type, PyObject *args, PyObject *kwds){
+	pyORBIT_Object* self;
+	self = (pyORBIT_Object *) type->tp_alloc(type, 0);
+	self->cpp_obj = NULL;
+	return (PyObject *) self;
+}
+
+/** Implementation of the __init__ method */
+static int BunchTuneAnalysis4D_init(pyORBIT_Object *self, PyObject *args, PyObject *kwds){
+	self->cpp_obj =  new BunchTuneAnalysis4D();
+	((BunchTuneAnalysis4D*) self->cpp_obj)->setPyWrapper((PyObject*) self);
+	return 0;
+}
+
+/** Analyzes the bunch. */
+static PyObject* BunchTuneAnalysis4D_analyzeBunch(PyObject *self, PyObject *args){
+	BunchTuneAnalysis4D* cpp_BunchTuneAnalysis4D = (BunchTuneAnalysis4D*)((pyORBIT_Object*) self)->cpp_obj;
+	PyObject* pyBunch;
+	if(!PyArg_ParseTuple(args,"O:analyzeBunch",&pyBunch)){
+		ORBIT_MPI_Finalize("BunchTuneAnalysis4D - analyzeBunch(Bunch* bunch) - parameter are needed.");
+	}
+	PyObject* pyORBIT_Bunch_Type = wrap_orbit_bunch::getBunchType("Bunch");
+	if(!PyObject_IsInstance(pyBunch,pyORBIT_Bunch_Type)){
+		ORBIT_MPI_Finalize("BunchTuneAnalysis4D - analyzeBunch(Bunch* bunch) - method needs a Bunch.");
+	}
+	Bunch* cpp_bunch = (Bunch*) ((pyORBIT_Object*)pyBunch)->cpp_obj;
+	cpp_BunchTuneAnalysis4D->analyzeBunch(cpp_bunch);
+	Py_INCREF(Py_None);
+	return Py_None;
+}
+
+/** Sets normalization matrix. */
+static PyObject* BunchTuneAnalysis4D_setMatrix(PyObject *self, PyObject *args){
+	BunchTuneAnalysis4D* cpp_BunchTuneAnalysis4D = (BunchTuneAnalysis4D*)((pyORBIT_Object*) self)->cpp_obj;
+	double dummy;
+	if(!PyArg_ParseTuple(args,"d:setMatrix",&dummy)){
+		ORBIT_MPI_Finalize("BunchTuneAnalysis4D - setMatrix(double dummy) - parameters are needed.");
+	}
+	cpp_BunchTuneAnalysis4D->setMatrix(dummy);
+	Py_INCREF(Py_None);
+	return Py_None;
+}
+
+// Destructor for python BunchTuneAnalysis4D class (__del__ method).
+static void BunchTuneAnalysis4D_del(pyORBIT_Object* self){
+	//std::cerr<<"The BunchTuneAnalysis4D __del__ has been called!"<<std::endl;
+	delete ((BunchTuneAnalysis4D*)self->cpp_obj);
+	self->ob_base.ob_type->tp_free((PyObject*)self);
+}
+
+// Definition of the methods of the python BunchTuneAnalysis4D wrapper class.
+// They will be available from python level.
+static PyMethodDef BunchTuneAnalysis4DClassMethods[] = {
+	{ "analyzeBunch", BunchTuneAnalysis4D_analyzeBunch, METH_VARARGS,"Analyzes the bunch."},
+	{ "setMatrix", BunchTuneAnalysis4D_assignTwiss, METH_VARARGS,"Sets normalization matrix."},
+	{NULL}
+};
+
+// Definition of the memebers of the python BunchTwissAnalysis wrapper class.
+// They will be vailable from python level.
+static PyMemberDef BunchTuneAnalysis4DClassMembers [] = {
+	{NULL}
+};
+
+// New python BunchTwissAnalysis wrapper type definition.
+static PyTypeObject pyORBIT_BunchTuneAnalysis4D_Type = {
+	PyVarObject_HEAD_INIT(NULL, 0)
+	"BunchTuneAnalysis4D", /*tp_name*/
+	sizeof(pyORBIT_Object), /*tp_basicsize*/
+	0, /*tp_itemsize*/
+	(destructor) BunchTuneAnalysis4D_del , /*tp_dealloc*/
+	0, /*tp_print*/
+	0, /*tp_getattr*/
+	0, /*tp_setattr*/
+	0, /*tp_compare*/
+	0, /*tp_repr*/
+	0, /*tp_as_number*/
+	0, /*tp_as_sequence*/
+	0, /*tp_as_mapping*/
+	0, /*tp_hash */
+	0, /*tp_call*/
+	0, /*tp_str*/
+	0, /*tp_getattro*/
+	0, /*tp_setattro*/
+	0, /*tp_as_buffer*/
+	Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /*tp_flags*/
+	"The BunchTuneAnalysis4D python wrapper", /* tp_doc */
+	0, /* tp_traverse */
+	0, /* tp_clear */
+	0, /* tp_richcompare */
+	0, /* tp_weaklistoffset */
+	0, /* tp_iter */
+	0, /* tp_iternext */
+	BunchTuneAnalysis4DClassMethods, /* tp_methods */
+	BunchTuneAnalysis4DClassMembers, /* tp_members */
+	0, /* tp_getset */
+	0, /* tp_base */
+	0, /* tp_dict */
+	0, /* tp_descr_get */
+	0, /* tp_descr_set */
+	0, /* tp_dictoffset */
+	(initproc) BunchTuneAnalysis4D_init, /* tp_init */
+	0, /* tp_alloc */
+	BunchTuneAnalysis4D_new, /* tp_new */
+};
+
+// Initialization of the pyBunchTwissAnalysis4D class.
+void initbunchtuneanalysis4d(PyObject* module){
+	if (PyType_Ready(&pyORBIT_BunchTuneAnalysis4D_Type) < 0) return;
+	Py_INCREF(&pyORBIT_BunchTuneAnalysis4D_Type);
+	PyModule_AddObject(module, "BunchTuneAnalysis4D", (PyObject *)&pyORBIT_BunchTuneAnalysis4D_Type);
+}
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+}
