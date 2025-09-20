194c78125c771340d9c65b3682a4da9b93534615
austin-hoover <austin.michael.hoover@gmail.com>
2025-09-20 17:05:11 -0400

    Add wrap_envelope.hh

---
+++ src/envelope/wrap_envelope.hh
@@ -0,0 +1,18 @@
+#ifndef WRAP_ENVELOPE_MODULE_HH_
+#define WRAP_ENVELOPE_MODULE_HH_
+
+#include "Python.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+namespace wrap_envelope {
+  PyMODINIT_FUNC initenvelope();
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*WRAP_ENVELOPE_MODULE_HH_*/
