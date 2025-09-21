911a2ec4f426c4372d92a826bb8013e0c72fa31c
austin-hoover <austin.michael.hoover@gmail.com>
2025-09-21 14:06:14 -0400

    Update plotting examples:

--- examples/Envelope/plot_kv.py
+++ examples/Envelope/plot_kv.py
@@ -1,9 +1,16 @@
+import os
+import pathlib
 import numpy as np
 import matplotlib.pyplot as plt
 
 from orbit.envelope import KVEnvelope
 
 plt.style.use("style.mplstyle")
+
+
+path = pathlib.Path(__file__)
+output_dir = os.path.join("outputs", path.stem)
+os.makedirs(output_dir, exist_ok=True)
 
 
 envelope = KVEnvelope(
@@ -23,8 +30,16 @@ fig, axs = plt.subplots(ncols=4, nrows=4, figsize=(4, 4), constrained_layout=Tru
 for i in range(4):
     for j in range(4):
         ax = axs[i, j]
-        ax.hist2d(x[:, j], x[:, i], bins=50, range=[limits[j], limits[i]])
+        ax.hist2d(x[:, j], x[:, i], bins=50, range=[limits[j], limits[i]], cmap="Blues")
 for ax in axs.flat:
     ax.set_xticks([])
     ax.set_yticks([])
+for i, dim in enumerate(["x", "xp", "y", "yp"]):
+    axs[i, 0].set_ylabel(dim)
+    axs[3, i].set_xlabel(dim)
+fig.align_ylabels()
+
+filename = "fig_corner.png"
+filename = os.path.join(output_dir, filename)
+plt.savefig(filename, dpi=300)
 plt.show()
