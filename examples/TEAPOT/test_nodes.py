fbf97436826338f20a303f6bee345f0e7ed85268
austin-hoover <austin.michael.hoover@gmail.com>
2025-10-20 13:59:08 -0400

    Add aperture test to test_nodes.py

--- examples/TEAPOT/test_nodes.py
+++ examples/TEAPOT/test_nodes.py
@@ -8,3 +8,11 @@ def test_drift():
     length = 1.0
     node = teapot.DriftTEAPOT(name="name", length=length)
     assert node.getLength() == length
+
+
+def test_aperture():
+    shape = 1
+    dim = (1, 2)
+    node = teapot.ApertureTEAPOT(name="name", shape=shape, dim=dim)
+    assert node.getParam("apertype") == shape
+    assert node.getParam("aperture") == dim
