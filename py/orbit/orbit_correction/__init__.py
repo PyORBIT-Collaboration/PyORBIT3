4c8b949db97d613826303be113e404f9442e1523
Wood, Tony <woodap@ornl.gov>
2025-12-17 18:02:08 -0500

    Guard against missing numpy under `py/orbit/obit_correction`, as well.

--- py/orbit/orbit_correction/__init__.py
+++ py/orbit/orbit_correction/__init__.py
@@ -8,11 +8,12 @@
 ##   addErrorNode - function to add one error
 ##                  node to the lattice
 
-from .orbit_correction import orbit
-from .orbit_correction import correction
-
-
 __all__ = []
-__all__.append("")
-__all__.append("orbit")
-__all__.append("correction")
+try:
+    from .orbit_correction import orbit
+    from .orbit_correction import correction
+    __all__.append("orbit")
+    __all__.append("correction")
+except ModuleNotFoundError:
+    pass
+
