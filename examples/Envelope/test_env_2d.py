6b8471179e3c17e38b85bfc3be34fbc5c3eb00ae
austin-hoover <austin.michael.hoover@gmail.com>
2026-01-05 12:09:38 -0500

    Start test script for envelope2D tracker

--- examples/Envelope/test_env_2d.py
+++ examples/Envelope/test_env_2d.py
@@ -1,1 +1,23 @@
+import math
+import numpy as np
+
+from orbit.core.bunch import Bunch
+from orbit.core.bunch import SyncParticle
 from orbit.envelope import Envelope2D
+from orbit.utils.consts import mass_proton
+
+
+mass = mass_proton  # [GeV]
+kin_energy = 1.000  # [GeV]
+
+bunch = Bunch()
+bunch.mass(mass)
+bunch.getSyncParticle().kinEnergy(kin_energy)
+
+cov_matrix = np.identity(4)
+cov_matrix[0, 0] = 10.0e-3 ** 2
+cov_matrix[2, 2] = 10.0e-3 ** 2
+
+envelope = Envelope2D(bunch=bunch, cov_matrix=cov_matrix)
+
+
