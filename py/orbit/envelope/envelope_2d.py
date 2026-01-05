1ef0323419bffa5f47a7413f376744e387e330b4
austin-hoover <austin.michael.hoover@gmail.com>
2026-01-05 12:04:09 -0500

    Envelope stores empty Bunch object
    This will be used to set the correct particle mass and energy.

--- py/orbit/envelope/envelope_2d.py
+++ py/orbit/envelope/envelope_2d.py
@@ -1,12 +1,12 @@
 import math
 import numpy as np
 
-from ..core.bunch import SyncParticle
+from ..core.bunch import Bunch
 
 
 class Envelope2D:
-    def __init__(self, sync_part: SyncParticle, cov_matrix: np.ndarray = None) -> None:
-        self.sync_part = sync_part
+    def __init__(self, bunch: Bunch, cov_matrix: np.ndarray = None) -> None:
+        self.bunch = bunch
 
         self.cov_matrix = cov_matrix
         if self.cov_matrix is None:
