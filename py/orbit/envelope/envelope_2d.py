1b037fb6832f9550f4020ef2a00ea55167422a46
austin-hoover <austin.michael.hoover@gmail.com>
2026-01-05 12:09:06 -0500

    Add track method to Envelope2D
    This method applies the linear transformation S -> M S M^T.

--- py/orbit/envelope/envelope_2d.py
+++ py/orbit/envelope/envelope_2d.py
@@ -11,3 +11,8 @@ class Envelope2D:
         self.cov_matrix = cov_matrix
         if self.cov_matrix is None:
             self.cov_matrix = np.eye(4)
+
+    def track(self, matrix: np.ndarray) -> None:
+        S = self.cov_matrix
+        M = matrix[:4, :4]
+        self.cov_matrix = np.linalg.multi_dot([M, S, M.T])
