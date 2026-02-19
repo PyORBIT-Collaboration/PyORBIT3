2c59ab10b516308497db55b6e4da9ee87c958dca
Austin Hoover <austin.michael.hoover@gmail.com>
2026-02-19 13:23:56 -0500

    Keep working on gradient calc

--- src/spacecharge/LSpaceChargeCalc.cc
+++ src/spacecharge/LSpaceChargeCalc.cc
@@ -119,71 +119,110 @@ void LSpaceChargeCalc::trackBunch(Bunch *bunch) {
     zGrid->binBunchSmoothedByParticle(bunch);
     zGrid->synchronizeMPI(bunch->getMPI_Comm_Local());
 
-    // FFT the beam density.
-    for (int i = 0; i < nBins; i++) {
-        _in[i][0] = zGrid->getValueOnGrid(i);
-        _in[i][1] = 0.;
+    if (useGrad == 0) {
+
+        // FFT the beam density.
+        for (int i = 0; i < nBins; i++) {
+            _in[i][0] = zGrid->getValueOnGrid(i);
+            _in[i][1] = 0.;
+        }
+
+        fftw_execute(_plan);
+
+        // Find the magnitude and phase
+        _fftmagnitude[0] = _out[0][0] / (double)nBins;
+        _fftphase[0] = 0.;
+
+        for (int n = 1; n < nBins / 2; n++) {
+            realPart = _out[n][0] / ((double)nBins);
+            imagPart = _out[n][1] / ((double)nBins);
+            _fftmagnitude[n] = sqrt(realPart * realPart + imagPart * imagPart);
+            _fftphase[n] = atan2(imagPart, realPart);
+        }
+
+        // Space charge contribution to impedance.
+        SyncPart *sp = bunch->getSyncPart();
+
+        // Set zero if useSpaceCharge = 0.
+        double zSpaceCharge_n = 0.;
+
+        // Otherwise, set positive since space charge is capacitive (Chao convention).
+        if (useSpaceCharge != 0) {
+            double mu_0 = 4.0 * OrbitConst::PI * 1.e-07; // permeability of free space
+            double _z_0 = OrbitConst::c * mu_0;
+
+            zSpaceCharge_n = _z_0 * (1.0 + 2.0 * log(b_a)) /
+                            (2 * sp->getBeta() * sp->getGamma() * sp->getGamma());
+        }
+
+        for (int n = 1; n < nBins / 2; n++) {
+            realPart = std::real(_zImped_n[n]);
+            imagPart = std::imag(_zImped_n[n]) + zSpaceCharge_n;
+            _z[n] = n * sqrt(realPart * realPart + imagPart * imagPart);
+            _chi[n] = atan2(imagPart, realPart);
+        }
+
+        // Convert charge to current for a single macroparticle per unit bin length.
+        double charge2current = bunch->getCharge() * bunch->getMacroSize() * OrbitConst::elementary_charge_MKS * sp->getBeta() * OrbitConst::c / (length / nBins);
+
+        // Calculate and add the kick to macroparticles.
+        double kick, angle, position;
+
+        // Convert particle longitudinal coordinate to phi.
+        double philocal;
+        double z;
+        double **coords = bunch->coordArr();
+        for (int j = 0; j < bunch->getSize(); j++) {
+            z = bunch->z(j);
+            philocal = (z / length) * 2 * OrbitConst::PI;
+
+            // Handle cases where the longitudinal coordinate is
+            // outside of the user-specified length
+            if (philocal < -OrbitConst::PI)
+                philocal += 2 * OrbitConst::PI;
+            if (philocal > OrbitConst::PI)
+                philocal -= 2 * OrbitConst::PI;
+
+            double dE = _kick(philocal) * (-1e-9) * bunch->getCharge() * charge2current;
+            if (j == 0) {
+                std::cout << "debug j= " << j << " dE = " << dE << "\n"; 
+            }
+            coords[j][5] += dE;
+        }
+    }
+    else {
+        double z;
+        double **coords = bunch->coordArr();
+
+        SyncPart *sp = bunch->getSyncPart();
+        double gamma = sp->getGamma();
+        double factor = length * bunch->getClassicalRadius() * bunch->getMass() / (gamma * gamma);
+
+        double density_gradient;
+        double dE;
+        for (int j = 0; j < bunch->getSize(); j++) {
+            z = bunch->z(j);
+            // z = bunch->z(j) * gamma;  // rest frame
+
+            // Handle cases where the longitudinal coordinate is
+            // outside of the user-specified length
+            // ...
+
+            if (smooth > 0) {
+                zGrid->calcGradientSmoothed(z, density_gradient);
+            }
+            else {
+                zGrid->calcGradient(z, density_gradient);
+            }
+            double dE = -factor * density_gradient;
+            dE = dE * 1e9; // ?
+            if (j == 0) {
+                std::cout << "debug j= " << j << " dE = " << dE << "\n"; 
+            }
+            coords[j][5] += dE;
+        }
     }
 
-    fftw_execute(_plan);
-
-    // Find the magnitude and phase
-    _fftmagnitude[0] = _out[0][0] / (double)nBins;
-    _fftphase[0] = 0.;
-
-    for (int n = 1; n < nBins / 2; n++) {
-        realPart = _out[n][0] / ((double)nBins);
-        imagPart = _out[n][1] / ((double)nBins);
-        _fftmagnitude[n] = sqrt(realPart * realPart + imagPart * imagPart);
-        _fftphase[n] = atan2(imagPart, realPart);
-    }
-
-    // Space charge contribution to impedance.
-    SyncPart *sp = bunch->getSyncPart();
-
-    // Set zero if useSpaceCharge = 0.
-    double zSpaceCharge_n = 0.;
-
-    // Otherwise, set positive since space charge is capacitive (Chao convention).
-    if (useSpaceCharge != 0) {
-        double mu_0 = 4.0 * OrbitConst::PI * 1.e-07; // permeability of free space
-        double _z_0 = OrbitConst::c * mu_0;
-
-        zSpaceCharge_n = _z_0 * (1.0 + 2.0 * log(b_a)) /
-                         (2 * sp->getBeta() * sp->getGamma() * sp->getGamma());
-    }
-
-    for (int n = 1; n < nBins / 2; n++) {
-        realPart = std::real(_zImped_n[n]);
-        imagPart = std::imag(_zImped_n[n]) + zSpaceCharge_n;
-        _z[n] = n * sqrt(realPart * realPart + imagPart * imagPart);
-        _chi[n] = atan2(imagPart, realPart);
-    }
-
-    // Convert charge to current for a single macroparticle per unit bin length.
-    double charge2current = bunch->getCharge() * bunch->getMacroSize() * OrbitConst::elementary_charge_MKS * sp->getBeta() * OrbitConst::c / (length / nBins);
-
-    // Calculate and add the kick to macroparticles.
-    double kick, angle, position;
-
-    // Convert particle longitudinal coordinate to phi.
-    double philocal;
-    double z;
-    double **coords = bunch->coordArr();
-    for (int j = 0; j < bunch->getSize(); j++) {
-        z = bunch->z(j);
-        philocal = (z / length) * 2 * OrbitConst::PI;
-
-        // Handle cases where the longitudinal coordinate is
-        // outside of the user-specified length
-        if (philocal < -OrbitConst::PI)
-            philocal += 2 * OrbitConst::PI;
-        if (philocal > OrbitConst::PI)
-            philocal -= 2 * OrbitConst::PI;
-
-        double dE = _kick(philocal) * (-1e-9) * bunch->getCharge() * charge2current;
-        coords[j][5] += dE;
-    }
 }
 
 
