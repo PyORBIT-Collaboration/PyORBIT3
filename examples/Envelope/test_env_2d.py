3d0d65cb7e58328126b330c12830e3678dbc9d46
Austin Hoover <austin.michael.hoover@gmail.com>
2026-04-24 11:32:35 -0400

    Update example

--- examples/Envelope/test_env_2d.py
+++ examples/Envelope/test_env_2d.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from orbit.core.bunch import Bunch
 from orbit.core.bunch import SyncParticle
-from orbit.envelope import Envelope2D
+from orbit.envelope import Envelope
 from orbit.utils.consts import mass_proton
 
 
@@ -14,10 +14,11 @@ bunch = Bunch()
 bunch.mass(mass)
 bunch.getSyncParticle().kinEnergy(kin_energy)
 
-cov_matrix = np.identity(4)
+cov_matrix = np.identity(6)
 cov_matrix[0, 0] = 10.0e-3 ** 2
 cov_matrix[2, 2] = 10.0e-3 ** 2
+cov_matrix[4, 4] = 50.0
 
-envelope = Envelope2D(bunch=bunch, cov_matrix=cov_matrix)
+envelope = Envelope(cov_matrix=cov_matrix)
 
 
