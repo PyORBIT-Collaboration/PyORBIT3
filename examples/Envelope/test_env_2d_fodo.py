Correct formula for 3D space charge matrix

examples/Envelope/test_env_2d_fodo.py
@@ -42,14 +42,12 @@ parser.add_argument("--zrms", type=float, default=5.0)
 parser.add_argument("--kin-energy", type=float, default=0.025)
 parser.add_argument("--intensity", type=float, default=5e10)
 
-parser.add_argument(
-    "--dist-name", type=str, default="kv", choices=["kv", "waterbag", "gauss"]
-)
-parser.add_argument("--dist-mismatch-x", type=float, default=0.0)
-parser.add_argument("--dist-mismatch-y", type=float, default=0.0)
-parser.add_argument("--dist-offset-x", type=float, default=0.0)
-parser.add_argument("--dist-offset-y", type=float, default=0.0)
-parser.add_argument("--dist-tilt", action="store_true")
+parser.add_argument("--dist", type=str, default="kv", choices=["kv", "waterbag", "gauss"])
+parser.add_argument("--mismatch-x", type=float, default=0.0)
+parser.add_argument("--mismatch-y", type=float, default=0.0)
+parser.add_argument("--offset-x", type=float, default=0.0)
+parser.add_argument("--offset-y", type=float, default=0.0)
+parser.add_argument("--tilt", action="store_true")
 
 parser.add_argument("--nslice", type=int, default=10)
 parser.add_argument("--kq", type=float, default=0.25)
@@ -120,20 +118,20 @@ cov_matrix[4, 4] = args.zrms**2
 cov_matrix[5, 5] = 0.0
 
 # Tilt
-if args.dist_tilt:
+if args.tilt:
     rot_matrix = np.identity(6)
     rot_matrix[:4, :4] = build_rotation_matrix_xy(angle=(0.15 * math.pi))
     cov_matrix = np.linalg.multi_dot([rot_matrix, cov_matrix, rot_matrix.T])
 
 # Mismatch
-cov_matrix[0, 0] *= (1.0 + args.dist_mismatch_x) ** 2
-cov_matrix[2, 2] *= (1.0 + args.dist_mismatch_y) ** 2
+cov_matrix[0, 0] *= (1.0 + args.mismatch_x) ** 2
+cov_matrix[2, 2] *= (1.0 + args.mismatch_y) ** 2
 cov_matrix_init = np.copy(cov_matrix)
 
 # Offset
 centroid_init = np.zeros(6)
-centroid_init[0] += args.dist_offset_x
-centroid_init[2] += args.dist_offset_y
+centroid_init[0] += args.offset_x
+centroid_init[2] += args.offset_y
 
 # Create envelope
 envelope = Envelope(
@@ -182,19 +180,16 @@ histories["envelope"] = copy.deepcopy(history)
 
 print("TRACK BUNCH")
 
-# Generate particles from KV distribution + uniform longitudinal distribution.
 rng = np.random.default_rng()
 
 bunch_coords = np.zeros((args.nparts, 6))
-bunch_coords[:, :4] = gen_dist(args.nparts, cov_matrix_init[0:4, 0:4], args.dist_name)
+bunch_coords[:, :4] = gen_dist(n=args.nparts, cov_matrix=cov_matrix_init[0:4, 0:4], name=args.dist)
 bunch_coords[:, 4] = 2.0 * rng.uniform(-args.zrms, args.zrms, size=args.nparts)
 bunch_coords += centroid_init[None, :6]
 
 for i in range(bunch_coords.shape[0]):
     bunch.addParticle(*bunch_coords[i])
 
-
-# Add space charge nodes to lattice.
 if args.sc:
     sc_calc = SpaceChargeCalc2p5D(128, 128, 1)
     sc_path_length_min = 1.00e-06
@@ -203,8 +198,6 @@ if args.sc:
     bunch_size = bunch.getSizeGlobal()
     bunch.macroSize(args.intensity / bunch_size)
 
-
-# Track bunch through lattice.
 history = {"xrms": [], "yrms": [], "xavg": [], "yavg": []}
 for turn in range(args.turns):
     if turn > 0:

examples/Envelope/test_env_3d_drift.py
@@ -39,7 +39,7 @@ plt.style.use("style.mplstyle")
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--kin-energy", type=float, default=0.025)
-parser.add_argument("--intensity", type=float, default=4e9)
+parser.add_argument("--intensity", type=float, default=1e11)
 
 parser.add_argument("--xrms", type=float, default=0.010)
 parser.add_argument("--yrms", type=float, default=0.010)
@@ -48,8 +48,9 @@ parser.add_argument("--zrms", type=float, default=0.010)
 parser.add_argument("--nslice", type=int, default=10)
 parser.add_argument("--length", type=float, default=0.1)
 parser.add_argument("--turns", type=int, default=20)
+parser.add_argument("--sc-grid", type=int, default=128)
 
-parser.add_argument("--nparts", type=int, default=100_000)
+parser.add_argument("--nparts", type=int, default=500_000)
 parser.add_argument("--sc", type=int, default=0)
 args = parser.parse_args()
 
@@ -83,9 +84,9 @@ sync_part = bunch.getSyncParticle()
 sync_part.kinEnergy(args.kin_energy)
 
 cov_matrix_init = np.zeros((6, 6))
-cov_matrix_init[0, 0] = 0.010 ** 2
-cov_matrix_init[2, 2] = 0.010 ** 2
-cov_matrix_init[4, 4] = (0.010 / sync_part.gamma()) ** 2
+cov_matrix_init[0, 0] = args.xrms ** 2
+cov_matrix_init[2, 2] = args.yrms ** 2
+cov_matrix_init[4, 4] = (args.zrms / sync_part.gamma()) ** 2
 
 centroid_init = np.zeros(6)
 
@@ -96,7 +97,6 @@ envelope = Envelope(
     intensity=args.intensity,
 )
 
-
 # Track envelope
 # ------------------------------------------------------------------------------
 
@@ -104,7 +104,7 @@ print("TRACK ENVELOPE")
 
 tracker = EnvelopeTracker(lattice, space_charge=("3d" if args.sc else None))
 
-history = {"xrms": [], "yrms": [], "xavg": [], "yavg": []}
+history = {"xrms": [], "yrms": [], "zrms": []}
 for turn in range(args.turns):
     if turn > 0:
         tracker.track(envelope)
@@ -114,17 +114,13 @@ for turn in range(args.turns):
 
     xrms = 1000.0 * math.sqrt(cov_matrix[0, 0])
     yrms = 1000.0 * math.sqrt(cov_matrix[2, 2])
-    xavg = 1000.0 * centroid[0]
-    yavg = 1000.0 * centroid[2]
-
-    print(
-        f"turn={turn} xrms={xrms:0.3f} yrms={yrms:0.3f} xavg={xavg:0.3f} yavg={yavg:0.3f}"
-    )
+    zrms = 1000.0 * math.sqrt(cov_matrix[4, 4]) * envelope.gamma()
 
     history["xrms"].append(xrms)
     history["yrms"].append(yrms)
-    history["xavg"].append(xavg)
-    history["yavg"].append(yavg)
+    history["zrms"].append(zrms)
+
+    print(f"turn={turn} xrms={xrms:0.3f} yrms={yrms:0.3f} zrms={zrms:0.3f}")
 
 histories = {}
 histories["envelope"] = copy.deepcopy(history)
@@ -148,13 +144,11 @@ size_global = bunch.getSizeGlobal()
 bunch.macroSize(args.intensity / size_global)
 
 if args.sc:
-    sc_calc = SpaceChargeCalc3D(64, 64, 64)
-    sc_min_path_length = 0.01
-    sc_nodes = setSC3DAccNodes(lattice, sc_min_path_length, sc_calc)
+    sc_calc = SpaceChargeCalc3D(args.sc_grid, args.sc_grid, args.sc_grid)
+    sc_path_length_min = 0.01
+    sc_nodes = setSC3DAccNodes(lattice, sc_path_length_min, sc_calc)
 
-
-# Track bunch through lattice.
-history = {"xrms": [], "yrms": [], "xavg": [], "yavg": []}
+history = {"xrms": [], "yrms": [], "zrms": []}
 for turn in range(args.turns):
     if turn > 0:
         lattice.trackBunch(bunch)
@@ -170,17 +164,13 @@ for turn in range(args.turns):
 
     xrms = 1000.0 * math.sqrt(cov_matrix[0, 0])
     yrms = 1000.0 * math.sqrt(cov_matrix[2, 2])
-    xavg = 1000.0 * twiss_calc.getAverage(0)
-    yavg = 1000.0 * twiss_calc.getAverage(2)
-
-    print(
-        f"turn={turn} xrms={xrms:0.3f} yrms={yrms:0.3f} xavg={xavg:0.3f} yavg={yavg:0.3f}"
-    )
+    zrms = 1000.0 * math.sqrt(cov_matrix[4, 4]) * bunch.getSyncParticle().gamma()
 
     history["xrms"].append(xrms)
     history["yrms"].append(yrms)
-    history["xavg"].append(xavg)
-    history["yavg"].append(yavg)
+    history["zrms"].append(zrms)
+
+    print(f"turn={turn} xrms={xrms:0.3f} yrms={yrms:0.3f} zrms={zrms:0.3f}")
 
 histories["bunch"] = copy.deepcopy(history)
 
@@ -200,7 +190,7 @@ for key in histories["envelope"]:
     print("avg_abs_delta:", np.mean(np.abs(deltas)))
 
 # Plot rms bunch sizes
-for key in ["xrms", "yrms"]:
+for key in ["xrms", "yrms", "zrms"]:
     fig, ax = plt.subplots(figsize=(5, 3))
     for i, model in enumerate(["envelope", "bunch"]):
         color = ["black", "red"][i]
@@ -212,21 +202,6 @@ for key in ["xrms", "yrms"]:
     ax.legend(loc="upper right")
     plt.savefig(os.path.join(output_dir, f"fig_{key}"))
     plt.close()
-
-# Plot centroids
-for key in ["xavg", "yavg"]:
-    fig, ax = plt.subplots(figsize=(5, 3))
-    for i, model in enumerate(["envelope", "bunch"]):
-        color = ["black", "red"][i]
-        lw = [None, 0][i]
-        ax.plot(histories[model][key], marker=".", lw=lw, color=color, label=model)
-    ax.set_ylim(-5.0, 5.0)
-    ax.set_xlabel("Turn")
-    ax.set_ylabel("AVG [mm]")
-    ax.legend(loc="upper right")
-    plt.savefig(os.path.join(output_dir, f"fig_{key}"))
-    plt.close()
-
 
 # Collect bunch/envelope data on final turn.
 particles = collect_bunch(bunch)["coords"]
@@ -241,7 +216,6 @@ env_centroid[:4] *= 1000.0
 xmax = 4.0 * np.std(particles, axis=0)
 limits = list(zip(-xmax, xmax))
 labels = ["x [mm]", "xp [mrad]", "y [mm]", "yp [mrad]", "z [m]", "dE [GeV]"]
-
 
 # Plot x-x'
 fig, ax = plt.subplots(figsize=(4, 4))

py/orbit/envelope/envelope.py
@@ -21,16 +21,7 @@ EXIT = AccNode.EXIT
 BEFORE = AccNode.BEFORE
 AFTER = AccNode.AFTER
 
-
-def calc_perveance_2d(gamma: float, line_density: float) -> float:
-    classical_proton_radius = 1.534697049469832e-18  # [m]
-    beta = np.sqrt(1.0 - (1.0 / gamma) ** 2)
-    return (2.0 * classical_proton_radius * line_density) / (beta**2 * gamma**3)
-
-
-def calc_perveance_3d(gamma: float, mass: float, total_charge: float) -> float:
-    bg2 = gamma**2 - 1.0
-    return total_charge * (1.0e-7 * speed_of_light**2) * (1.0 / (gamma * bg2)) * abs(charge_electron) / mass
+CLASSICAL_PROTON_RADIUS = 1.534697049469832e-18  # [m]
 
 
 def build_diag_matrix_from_xyz_eig(eigenvectors: np.ndarray) -> np.ndarray:
@@ -41,53 +32,6 @@ def build_diag_matrix_from_xyz_eig(eigenvectors: np.ndarray) -> np.ndarray:
             col = j * 2
             A[row, col] = A[row + 1, col + 1] = eigenvectors[i, j]
     return A
-
-
-def build_sc_matrix_2d(
-    cov_xx: float,
-    cov_yy: float,
-    perveance: float,
-    length: float,
-) -> np.ndarray:
-    """Return 7 x 7 space charge matrix for upright 2D ellipsoid."""
-    r_x = 2.0 * math.sqrt(cov_xx)
-    r_y = 2.0 * math.sqrt(cov_yy)
-    kappa_x = 2.0 * perveance / (r_x * (r_x + r_y))
-    kappa_y = 2.0 * perveance / (r_y * (r_x + r_y))
-    
-    matrix = np.identity(7)
-    matrix[1, 0] = kappa_x * length
-    matrix[3, 2] = kappa_y * length
-    return matrix
-
-
-def build_sc_matrix_3d(
-    cov_xx: float, 
-    cov_yy: float, 
-    cov_zz: float, 
-    perveance: float, 
-    length: float,
-) -> np.ndarray:    
-    """Return 7 x 7 space charge matrix for upright 3D ellipsoid."""
-    
-    scale_factor = 5.0 ** 1.5
-    RDx = scipy.special.elliprd(cov_yy, cov_zz, cov_xx) / scale_factor
-    RDy = scipy.special.elliprd(cov_zz, cov_xx, cov_yy) / scale_factor
-    RDz = scipy.special.elliprd(cov_xx, cov_yy, cov_zz) / scale_factor
-  
-    kappa_x = perveance * RDx
-    kappa_y = perveance * RDy
-    kappa_z = perveance * RDz
-
-    matrix = np.identity(7)
-    matrix[1, 0] = kappa_x * length
-    matrix[3, 2] = kappa_y * length
-    matrix[5, 4] = kappa_z * length
-
-    # PyORBIT uses dE, not z', so need to calculate energy
-    # kick from q * E_z * length.
-    
-    return matrix
 
 
 class Envelope:
@@ -137,33 +81,31 @@ class Envelope:
         self.set_intensity(intensity)
 
     def set_intensity(self, intensity: float) -> None:
+        factor = 2.0 * CLASSICAL_PROTON_RADIUS / (self.beta()**2 * self.gamma()**3)
+        bunch_length = 4.0 * self.rms(axis=4)
+
         self.intensity = intensity
-        cov_matrix = self.cov()
-        length = 4.0 * math.sqrt(cov_matrix[4, 4])  # assume uniform density
-        self.perveance_2d = calc_perveance_2d(
-            gamma=self.gamma(),
-            line_density=(self.intensity / length),
-        )
-        self.perveance_3d = calc_perveance_3d(
-            gamma=self.gamma(),
-            mass=(self.mass() * 1e9),
-            total_charge=(self.intensity * charge_electron),
-        )
+        self.perveance_3d = factor * intensity
+        self.perveance_2d = factor * intensity / bunch_length
 
     def gamma(self) -> float:
         return self.sync_part.gamma()
+
+    def beta(self) -> float:
+        return self.sync_part.beta()
     
     def mass(self) -> float:
         return self.sync_part.mass()
 
     def centroid(self) -> np.ndarray:
-        return self.matrix[0:6, 6]
+        return np.copy(self.matrix[0:6, 6])
 
     def cov(self) -> np.ndarray:
-        return self.matrix[0:6, 0:6]
+        return np.copy(self.matrix[0:6, 0:6])
 
-    def rms(self) -> np.ndarray:
-        return np.sqrt(np.diag(self.cov()))
+    def rms(self, axis: int = None) -> float | np.ndarray:
+        rms_arr = np.sqrt(np.diag(self.cov()))
+        return rms_arr[axis]
 
     def apply_transfer_matrix(self, transfer_matrix: np.ndarray) -> None:
         self.matrix = np.linalg.multi_dot([transfer_matrix, self.matrix, transfer_matrix.T])
@@ -180,16 +122,21 @@ class Envelope:
         cov_matrix = self.cov()
         cov_matrix_proj = proj_cov_matrix(cov_matrix, axis=(0, 2))
         
-        eig_res = np.linalg.eig(cov_matrix_proj)
+        cov_eig_res = np.linalg.eig(cov_matrix_proj)
+        cov_eig_vals = cov_eig_res.eigenvalues
+        cov_eig_vecs = cov_eig_res.eigenvectors
 
-        M = build_sc_matrix_2d(
-            cov_xx=eig_res.eigenvalues[0], 
-            cov_yy=eig_res.eigenvalues[1], 
-            perveance=self.perveance_2d, 
-            length=length
-        )
+        rx = 2.0 * math.sqrt(cov_eig_vals[0])
+        ry = 2.0 * math.sqrt(cov_eig_vals[1])
 
-        A = build_diag_matrix_from_xyz_eig(eig_res.eigenvectors)
+        kappa_x = 2.0 * self.perveance_2d / (rx * (rx + ry))
+        kappa_y = 2.0 * self.perveance_2d / (ry * (rx + ry))
+
+        M = np.identity(7)
+        M[1, 0] = kappa_x * length
+        M[3, 2] = kappa_y * length
+
+        A = build_diag_matrix_from_xyz_eig(cov_eig_vecs)
         
         T = np.identity(7)
         T[0, -1] = centroid[0]
@@ -200,37 +147,54 @@ class Envelope:
         return np.linalg.multi_dot([V, M, V_inv])
     
     def sc_transfer_matrix_3d(self, length: float) -> np.ndarray:
-        # TEMP
-        #
-        # Calculate transfer matrix M from covariance matrix S in normalized
-        # coordinates. In the normalized coordinates, the beam is at rest with
-        # zero mean and diagonal x-y-z covariance matrix.
-        #
-        # The matrix V to get back to lab-frame coordinates is then
-        # V = LTA, where A is from the x-y-z covariance eigenvectors, T is a
-        # translation to the x-y-z centroid, and L is the Lorentz boost that
-        # scales z by 1 / gamma.
-
+        # Get centroid in rest frame.
         centroid = self.centroid()
         centroid[4] *= self.gamma()
 
+        # Get covariance matrix in rest frame.
         cov_matrix = self.cov()
         cov_matrix[4, 4] *= self.gamma()**2
 
+        # Project covariance matrix onto x-y-z plane.
         cov_matrix_proj = proj_cov_matrix(cov_matrix, axis=(0, 2, 4))
-        
+
+        # Calculate eigenvectors of x-y-z covariance matrix.
         eig_res = np.linalg.eig(cov_matrix_proj)
 
-        M = build_sc_matrix_3d(
-            cov_xx=eig_res.eigenvalues[0], 
-            cov_yy=eig_res.eigenvalues[1], 
-            cov_zz=eig_res.eigenvalues[2],
-            perveance=self.perveance_3d,
-            length=length,
-        )   
+        # Extract diagonal x-y-z covariance matrix elements in diagonalized frame.
+        cov_xx = eig_res.eigenvalues[0]
+        cov_yy = eig_res.eigenvalues[1]
+        cov_zz = eig_res.eigenvalues[2]
+
+        # Compute linear matrix elements k_{x, y, z}.
+        # x' -> kappa_x * ds * x'
+        # y' -> kappa_y * ds * y'
+        # z' -> kappa_z * ds * z'
+        factor_x = 0.5 * self.perveance_3d / ((5.0 * cov_xx) ** 1.5)
+        factor_y = 0.5 * self.perveance_3d / ((5.0 * cov_yy) ** 1.5)
+        factor_z = 0.5 * self.perveance_3d / ((5.0 * cov_zz) ** 1.5)
+
+        RDx = scipy.special.elliprd((cov_yy / cov_xx), (cov_zz / cov_xx), 1.0)
+        RDy = scipy.special.elliprd((cov_xx / cov_yy), (cov_zz / cov_yy), 1.0)
+        RDz = scipy.special.elliprd((cov_xx / cov_zz), (cov_yy / cov_zz), 1.0)
+
+        kappa_x = factor_x * RDx  # [1 / m]
+        kappa_y = factor_y * RDy  # [1 / m]
+        kappa_z = factor_z * RDz  # [1 / m]
+
+        # Switch from z' to dE [GeV]
+        # z' = (dp / p) / gamma^2
+        # z' = (dE / E) / (gamma^2 * beta^2)
+        # z' = dE / (gamma^3 * beta^2 * m & c^2)
+        kappa_z *= self.gamma()**3 * self.beta()**2 * self.mass()  # [GeV / m]
+
+        M = np.identity(7)
+        M[1, 0] = kappa_x * length
+        M[3, 2] = kappa_y * length
+        M[5, 4] = kappa_z * length
 
         A = build_diag_matrix_from_xyz_eig(eig_res.eigenvectors)
-        
+
         T = np.identity(7)
         for i in (0, 2, 4):
             T[i, -1] = centroid[i]
@@ -272,6 +236,9 @@ class EnvelopeTracker:
                         matrix = envelope.sc_transfer_matrix_3d(length)
                     else:
                         raise ValueError(f"Invalid space charge model: {self.space_charge}")
+
+                    # print("debug space charge matrix")
+                    # print(matrix)
 
                     envelope.apply_transfer_matrix(matrix)
 