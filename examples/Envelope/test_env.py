Fix confusion on covariance vs. moments matrix

examples/Envelope/test_env.py
@@ -41,8 +41,6 @@ def track_and_compare_rms(
     kin_energy: float,
     cov_matrix: np.ndarray,
     nparts: int = 100_000,
-    rtol: float = 1e-5,
-    atol: float = 0,
     verbose: int = 1,
 ) -> dict:
     """Track bunch/envelope and compare rms beam sizes.
@@ -52,8 +50,7 @@ def track_and_compare_rms(
         kin_energy: Synchronous particle kinetic energy [GeV].
         cov_matrix: 6 x 6 covariance matrix.
         nparts: Number of particles in bunch.
-        rtol/atol: Relative/absolute tolerance on rms beam sizes (bunch vs. envelope).
-            Units are [mm, mrad].
+        rtol: Relative tolerance on rms beam sizes (bunch vs. envelope).
         verbose: Whether to print results.
     """
     cov_scale = 1e6
@@ -105,21 +102,29 @@ def track_and_compare_rms(
                 print("    env:   {}".format(data["env"]["rms"][key][i]))
                 print("    bunch: {}".format(data["bunch"]["rms"][key][i]))
 
-    for key in ["in", "out"]:
-        assert np.all(
-            np.isclose(
-                data["env"]["cov"][key], data["bunch"]["cov"][key], rtol=rtol, atol=atol
-            )
+    assert np.all(np.isclose(data["env"]["cov"]["in"], data["bunch"]["cov"]["in"]))
+
+    atol = np.ones(6)
+    atol[0:4] = 1e-3  # [mm mrad]
+    atol[4] = 1e-3  # [mm]
+    atol[5] = 1e-3  # [MeV]
+
+    for i in range(6):
+        assert np.isclose(
+            data["env"]["rms"]["out"][i],
+            data["bunch"]["rms"]["out"][i],
+            atol=atol[i],
+            rtol=0,
         )
 
 
 def make_default_cov_matrix(
-    rms_x: float = 0.001,
-    rms_xp: float = 0.001,
-    rms_y: float = 0.001,
-    rms_yp: float = 0.001,
-    rms_z: float = 0.001,
-    rms_dE: float = 0.00001,
+    rms_x: float = 1e-3,
+    rms_xp: float = 1e-3,
+    rms_y: float = 1e-3,
+    rms_yp: float = 1e-3,
+    rms_z: float = 1e-3,
+    rms_dE: float = 1e-5,
 ) -> np.ndarray:
     return np.diag(np.square([rms_x, rms_xp, rms_y, rms_yp, rms_z, rms_dE]))
 
@@ -162,10 +167,10 @@ def test_dipole(
 
 def test_kick(
     kin_energy: float = 0.0025,
-    length: float = 0.1,
+    length: float = 1.0,
     kx: float = 0.001,
     ky: float = 0.001,
-    dE: float = 0.0001,
+    dE: float = 0.00001,
     cov_matrix: np.ndarray = None,
 ) -> None:
     nodes = [KickTEAPOT(kx=kx, ky=ky, dE=dE, length=length)]
@@ -188,8 +193,8 @@ def test_tilt(
 
 
 if __name__ == "__main__":
-    # test_drift()
-    # test_quad()
-    # test_dipole()
-    test_kick()
+    test_drift()
+    test_quad()
+    test_dipole()
+    # test_kick()
 

py/orbit/envelope/envelope.py
@@ -46,12 +46,11 @@ class Envelope:
             and U is 6 x 1 "driving" vector. The augmented vector Y evolves according
             to Y -> NY, where N = [[M, U], [0, 1]] is a 7 x 7 matrix.
 
-            We track the 7 x 7 covariance matrix of Y:
-
-            R = <YY^T> = [[<XX^T>, <X>], [<X^T>, 1]],
-
-            which contains both the phase space covariance matrix and centroid vector.
-            R evolves according to R -> N R N^T.
+            Let S = <YY^T> = [[<XX^T>, <X>], [<X^T>, 1]] = [[R, C], [C^T, 1]]. Here
+            R = <XX^T> is the matrix of second moments, or "autocorrelation" matrix,
+            and C = <X> is the mean/centroid vector. (To get the covariance matrix:
+            <(X - C)(X - C)^T> = <XX^T> - <X><X>^T = R - CC^T.) S evolves according
+            to S -> N S N^T.
     """
 
     def __init__(
@@ -62,22 +61,21 @@ class Envelope:
         intensity: float = 0.0,
     ) -> None:
         self.sync_part = sync_part
+        self.dim = 6
 
         if centroid is None:
             centroid = np.zeros(6)
 
         if cov_matrix is None:
             cov_matrix = np.eye(6)
-
-        self.matrix = np.zeros((7, 7))
-        self.matrix[0:6, 0:6] = cov_matrix
-        self.matrix[0:6, 6] = centroid
-        self.matrix[6, 0:6] = centroid
-        self.matrix[6, 6] = 1.0
+            
+        self.moment_matrix = np.zeros((7, 7))
+        self.moment_matrix[:self.dim, :self.dim] = cov_matrix + np.outer(centroid, centroid)
+        self.moment_matrix[:self.dim, self.dim] = centroid
+        self.moment_matrix[self.dim, :self.dim] = centroid
+        self.moment_matrix[self.dim, self.dim] = 1.0
 
         self.intensity = 0.0
-        self.perveance_2d = 0.0
-        self.perveance_3d = 0.0
         self.set_intensity(intensity)
 
     def set_intensity(self, intensity: float) -> None:
@@ -99,24 +97,24 @@ class Envelope:
         return self.sync_part.mass()
 
     def centroid(self) -> np.ndarray:
-        return np.copy(self.matrix[0:6, 6])
+        return np.copy(self.moment_matrix[:self.dim, self.dim])
 
     def cov(self) -> np.ndarray:
-        return np.copy(self.matrix[0:6, 0:6])
+        autocorrelation_matrix = self.moment_matrix[:self.dim, :self.dim]
+        centroid = self.moment_matrix[:self.dim, self.dim]
+        return autocorrelation_matrix - np.outer(centroid, centroid)
 
     def rms(self, axis: int = None) -> float | np.ndarray:
         rms_arr = np.sqrt(np.diag(self.cov()))
         return rms_arr[axis]
 
-    def apply_transfer_matrix(self, transfer_matrix: np.ndarray) -> None:
-        self.matrix = np.linalg.multi_dot(
-            [transfer_matrix, self.matrix, transfer_matrix.T]
-        )
+    def apply_transfer_matrix(self, m: np.ndarray) -> None:
+        self.moment_matrix = np.linalg.multi_dot([m, self.moment_matrix, m.T])
 
-    def sample(self, n: int, dist: str = "kv") -> np.ndarray:
+    def sample(self, size: int, dist: str = "kv") -> np.ndarray:
         # Issue: covariance matrix is becoming non semi-positive definite,
         # giving error in cholesky decomposition.
-        particles = gen_dist(n=n, cov_matrix=self.cov(), name=dist)
+        particles = gen_dist(n=size, cov_matrix=self.cov(), name=dist)
         particles = particles + self.centroid()
         return particles
 

py/orbit/envelope/matrix.py
@@ -15,7 +15,7 @@ from ..teapot import BunchWrapTEAPOT
 from ..teapot import FringeFieldTEAPOT
 from ..teapot import MonitorTEAPOT
 from ..teapot import TurnCounterTEAPOT
-
+from ..teapot import MultipoleTEAPOT
 from ..utils import speed_of_light
 
 
@@ -146,16 +146,16 @@ class MatrixFactory:
 
     def translation_matrix(self, x: float = 0.0, y: float = 0.0, z: float = 0.0) -> np.ndarray:
         matrix = np.identity(7)
-        matrix[0, 6] = x
-        matrix[2, 6] = y
-        matrix[4, 6] = z
+        matrix[0, -1] = x
+        matrix[2, -1] = y
+        matrix[4, -1] = z
         return matrix
 
-    def kick_matrix(self, kx: float, ky: float, dE: float) -> np.ndarray:
+    def kick_matrix(self, kx: float = 0.0, ky: float = 0.0, kE: float = 0.0) -> np.ndarray:
         matrix = np.identity(7)
-        matrix[1, 6] = kx
-        matrix[3, 6] = ky
-        matrix[5, 6] = dE
+        matrix[1, -1] = kx
+        matrix[3, -1] = ky
+        matrix[5, -1] = kE
         return matrix
 
     def solenoid_matrix(self, length: float, B: float, sync_part: SyncParticle) -> np.ndarray:
@@ -192,12 +192,12 @@ class MatrixFactory:
             if node.waveform:
                 scale = node.waveform.getStrength()
 
-            kx = scale * node.getParam("kx") / nparts
-            ky = scale * node.getParam("ky") / nparts
-            dE = node.getParam("dE") / nparts
+            kx = scale * node.getParam("kx") / (nparts - 1)
+            ky = scale * node.getParam("ky") / (nparts - 1)
+            kE = node.getParam("dE") / (nparts - 1)
 
             return np.matmul(
-                self.kick_matrix(kx=kx, ky=ky, dE=dE),
+                self.kick_matrix(kx=kx, ky=ky, kE=kE),
                 self.drift_matrix(length=length, sync_part=sync_part)
             )
 
@@ -211,6 +211,8 @@ class MatrixFactory:
         else:
             if self.handle_unknown == "drift":
                 return self.drift_matrix(length=node.getLength(), sync_part=sync_part)
+
             elif self.handle_unknown == "fit":
                 raise NotImplementedError()
-            raise NotImplementedError("Unsupported node type: {}. See `handle_unknown` attribute.".format(type(node)))
+
+            raise NotImplementedError("Unsupported node: {}.".format(node))