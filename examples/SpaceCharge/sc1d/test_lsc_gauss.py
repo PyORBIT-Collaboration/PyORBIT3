666302a47af652467308f75e0a67743b7f9cf53b
austin-hoover <austin.michael.hoover@gmail.com>
2026-06-17 11:26:37 -0400

    Add analytic result for Gaussian distribution

--- examples/SpaceCharge/sc1d/test_lsc_gauss.py
+++ examples/SpaceCharge/sc1d/test_lsc_gauss.py
@@ -1,6 +1,8 @@
 import argparse
+import os
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 from orbit.core.bunch import Bunch
 from orbit.core.spacecharge import LSpaceChargeCalc
@@ -9,7 +11,11 @@ from orbit.core.spacecharge import Boundary2D
 from orbit.bunch_utils import collect_bunch
 from orbit.utils.consts import mass_proton
 
+plt.style.use("style.mplstyle")
+
+
 parser = argparse.ArgumentParser()
+parser.add_argument("--kin_energy", type=float, default=1.0)
 parser.add_argument("--b-a", type=float, default=3.0)
 parser.add_argument("--ring-length", type=float, default=250.0)
 parser.add_argument("--nmacros-min", type=int, default=0)
@@ -24,11 +30,18 @@ parser.add_argument("--grad", type=int, default=0)
 parser.add_argument("--modes", type=int, default=None)
 args = parser.parse_args()
 
+
+# Paths
+output_dir = "outputs"
+os.makedirs(output_dir, exist_ok=True)
+
 # Create bunch
 bunch = Bunch()
 bunch.mass(mass_proton)
-bunch.getSyncParticle().kinEnergy(1.0)
 bunch.macroSize(args.intensity / args.nparts)
+
+sync_part = bunch.getSyncParticle()
+sync_part.kinEnergy(args.kin_energy)
 
 # Generate particles
 rng = np.random.default_rng(0)
@@ -60,16 +73,37 @@ coords_in = collect_bunch(bunch)["coords"]
 sc_calc.trackBunch(bunch)
 coords_out = collect_bunch(bunch)["coords"]
 
+# Analytic result (assume zero mean Gaussian)
+g = 1.0 + 2.0 * np.log(args.b_a)
+factor = (
+    args.ring_length
+    * g
+    * bunch.classicalRadius()
+    * bunch.mass()
+    / (sync_part.gamma() ** 2)
+)
+z = args.ring_length * np.linspace(-0.5, 0.5, 1000)
+sigma = args.bunch_length_rms * 0.5
+density = np.sqrt(1.0 / (2.0 * np.pi * sigma**2)) * np.exp(-0.5 * (z / sigma) ** 2)
+density *= args.intensity
+density_grad = -(z / sigma**2) * density
+energy_kick = -factor * density_grad
+
+z_pred = z
+dE_pred = energy_kick
+
 # Plot position-energy distribution. The energy gain is proportional
 # to the longitudinal electric field.
-import matplotlib.pyplot as plt
-
-xmax = [0.5 * args.ring_length, 1.1 * np.max(coords_out[:, 5])]
+xmax = [0.5 * args.ring_length, 1.1 * 1000.0 * np.max(coords_out[:, 5])]
 xmax = np.array(xmax)
 limits = list(zip(-xmax, xmax))
 
 fig, ax = plt.subplots()
-ax.scatter(coords_out[:, 4], coords_out[:, 5], ec="none", s=1.0, c="black")
+ax.scatter(coords_out[:, 4], 1000.0 * coords_out[:, 5], ec="none", s=1.0, c="black")
+ax.plot(z_pred, 1000.0 * dE_pred, color="red", zorder=0)
 ax.set_xlim(limits[0])
 ax.set_ylim(limits[1])
+ax.set_xlabel("z [m]")
+ax.set_ylabel("dE [MeV]")
+plt.savefig(os.path.join(output_dir, "fig_lsc_gauss.png"), dpi=300)
 plt.show()
