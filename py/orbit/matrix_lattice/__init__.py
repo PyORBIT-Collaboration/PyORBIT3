a079b3318d79db78fea4e8c3321576e0c05141da
austin-hoover <austin.michael.hoover@gmail.com>
2026-06-23 22:57:50 -0400

    Remove commented lines

--- py/orbit/matrix_lattice/__init__.py
+++ py/orbit/matrix_lattice/__init__.py
@@ -4,19 +4,6 @@
 ## These classes use orbit::utils::matrix::Matrix C++ wrappers
 from .MATRIX_Lattice import MATRIX_Lattice
 from .BaseMATRIX import BaseMATRIX
-
-# from .analytic import get_dp_p_coeff
-# from .analytic import get_zp_coeff
-# from .analytic import convert_matrix_dp_p_to_dE
-# from .analytic import convert_matrix_zp_to_dE
-# from .analytic import drift_matrix
-# from .analytic import quad_matrix
-# from .analytic import bend_matrix
-# from .analytic import tilt_matrix
-# from .analytic import translation_matrix
-# from .analytic import kick_matrix
-# from .analytic import solenoid_matrix
-# from .analytic import cf_matrix
 from . import analytic
 
 
@@ -24,16 +11,3 @@ __all__ = []
 __all__.append("MATRIX_Lattice")
 __all__.append("BaseMATRIX")
 __all__.append("analytic")
-# __all__.append("get_dp_p_coeff")
-# __all__.append("get_zp_coeff")
-# __all__.append("convert_matrix_dp_p_to_dE")
-# __all__.append("convert_matrix_zp_to_dE")
-# __all__.append("drift_matrix")
-# __all__.append("quad_matrix")
-# __all__.append("bend_matrix")
-# __all__.append("tilt_matrix")
-# __all__.append("translation_matrix")
-# __all__.append("kick_matrix")
-# __all__.append("solenoid_matrix")
-# __all__.append("cf_matrix")
-#
